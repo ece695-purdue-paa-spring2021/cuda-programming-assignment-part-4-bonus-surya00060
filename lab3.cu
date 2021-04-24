409cede28d1fe0c2d1b172fddbcdb8b9f319e550
surya00060 <surya00060@gmail.com>
2021-04-24 16:02:33 -0400

    Bugs Fixed

--- lab3.cu
+++ lab3.cu
@@ -14,39 +14,15 @@
 #include "cpuLib.h"
 #include "cudaLib.cuh"
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) { 
 	std::string str;
 	int choice;
 
-	std::cout << "ECE 695 - Lab 2 \n";
-	std::cout << "Select application: \n";
-	std::cout << "  1 - CPU SAXPY \n";
-	std::cout << "  2 - GPU SAXPY \n";
-	std::cout << "  3 - CPU Monte-Carlo Pi \n";
-	std::cout << "  4 - GPU Monte-Carlo Pi \n";
-	std::cout << "  5 - Bytes-Image File Test \n";
-	std::cout << "  6 - Median Filter CPU \n";
-	std::cout << "  7 - Median Filter GPU \n";
-	std::cout << "  8 - Pool CPU \n";
-	std::cout << "  9 - Pool GPU \n";
-	std::cout << "  12 - Convolution on CPU \n";
-	std::cout << "  13 - Convolution on GPU \n";
-	std::cout << "  14 - Matrix Multiply on CPU \n";
-	std::cout << "  15 - Matrix Multiply on GPU \n";
-
-	std::cin >> choice;
-
-	std::cout << "\n";
-	std::cout << "Choice selected - " << choice << "\n\n";
-
-	PoolLayerArgs poolArgs;
-	MedianFilterArgs filArgs;
-	TensorShape inShape;
-
+	std::cout << "ECE 695 - Lab 4 Bonus\n";
 	std::cout << "Running AlexNet on GPU! \n\n";
 	runGpuAlexNet(argc, argv);	
 	std::cout << "\n\n ... Done!\n";
-	
+
 	return 0;
 }
 
